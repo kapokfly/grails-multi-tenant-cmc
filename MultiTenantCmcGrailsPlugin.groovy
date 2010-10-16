ce0a16f545ffe0d60f7056f13a5595f02710c0fa
sryan <scottryan@sryans-macbook-pro-17.local>
2010-10-16 10:49:13 -0600

    rename plugin

--- MultiTenantCmcGrailsPlugin.groovy
+++ MultiTenantCmcGrailsPlugin.groovy
@@ -1,50 +1,43 @@
-class MultiTenantCmcGrailsPlugin {
-    // the plugin version
-    def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.4 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [multiTenantCode:"1.0.0-SNAPSHOT]
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
-    ]
-
-    // TODO Fill in these fields
-    def author = "Your name"
-    def authorEmail = ""
-    def title = "Plugin summary/headline"
-    def description = '''\\
-Brief description of the plugin.
+class MultiTenantCmcGrailsPlugin
+{
+  // the plugin version
+  def version = "1.0.0-SNAPSHOT"
+  // the version or versions of Grails the plugin is designed for
+  def grailsVersion = "1.3.0 > *"
+  // the other plugins this plugin depends on
+  def dependsOn = [multiTenantCore: "1.0.0-SNAPSHOT"]
+  // resources that are excluded from plugin packaging
+  def pluginExcludes = [
+          "grails-app/views/error.gsp"
+  ]
+  def author = "Scott Ryan"
+  def authorEmail = "scryan@codehaus.org"
+  def title = "Multi-Tenant Central Management Console"
+  def description = '''\\
+This plugin provides a central point of management for a database per client implmentation of the multi tenant plugins.
 '''
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/multi-tenant-cmc"
+  // URL to the plugin's documentation
+  def documentation = "http://grails.org/plugin/multi-tenant-cmc"
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
-    }
+  def doWithWebDescriptor = { xml ->
+  }
 
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
+  def doWithSpring = {
+  }
 
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
+  def doWithDynamicMethods = { ctx ->
+  }
 
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
+  def doWithApplicationContext = { applicationContext ->
+  }
 
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
+  def onChange = { event ->
+    // watching is modified and reloaded. The event contains: event.source,
+    // event.application, event.manager, event.ctx, and event.plugin.
+  }
 
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
+  def onConfigChange = { event ->
+    // The event is the same as for 'onChange'.
+  }
 }
